5f5401e43bffc689ac87e5dc7f106800f32563ad
lucasojohn <lucas.ti.john@gmail.com>
2022-11-17 23:42:59 -0300

    Criados fix Ids para tela de cadastro e deleta cliente e variaveis

--- agencia/src/agencia/control/ClienteController.java
+++ agencia/src/agencia/control/ClienteController.java
@@ -5,7 +5,10 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -15,6 +18,18 @@ public class ClienteController implements Initializable {
     
     @FXML
     private Label label;
+    private TextField cpf;
+    private TextField nome;
+    private TextField dataNasc;
+    private RadioButton masculino;
+    private RadioButton feminino;
+    private TextField endereco;
+    private RadioButton contaCorrente;
+    private RadioButton contaPoupanca;
+    private TextField saldo;
+    private Button deletar;
+    private Button cancelar;
+    
     
     @FXML
     public void fechar(ActionEvent event){       
